18d450bbccb0d6878bb667fa6aad6fb7ada1b40a
jordan-nguyen <dizorder3@gmail.com>
2024-02-23 08:59:40 +0100

    UPD [main] import css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import '@/assets/tabler.min.css'
+import '@/assets/demo.min.css'
+import '@/assets/tabler-vendors.min.css'
 
 const app = createApp(App)
 
