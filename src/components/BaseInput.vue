97d84a7343919e1a8acb0fd0ff04b08aae78f333
jordan-nguyen <dizorder3@gmail.com>
2024-02-23 11:56:29 +0100

    UPD [login & register] dynamic

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  data() {
+    return {
+      showPassword: false,
+      password: null
+    }
+  },
+  computed: {
+    buttonLabel() {
+      return (this.showPassword) ? "Hide" : "Show";
+    }
+  },
+  methods: {
+    toggleShow() {
+      this.showPassword = !this.showPassword;
+    }
+  },
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    }
+  }
+}
+</script>
+
+<template>
+  <div v-if="label === 'Mot de passe'" class="mb-3">
+    <label class="form-label">{{ label }}</label>
+    <div class="input-group input-group-flat">
+      <input v-if="showPassword" type="text" name="text" class="form-control" :placeholder="placeholder" v-model="password">
+      <input v-else type="password" name="password" class="form-control" :placeholder="placeholder" v-model="password">
+      <span class="input-group-text">
+        <button @click="toggleShow" class="button" title="Show password" type="button"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
+          <svg v-if="showPassword" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye-off" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" /><path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" /><path d="M3 3l18 18" /></svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
+        </button>
+      </span>
+    </div>
+  </div>
+  <div v-else class="mb-3">
+    <label class="form-label">{{ label }}</label>
+    <input class="form-control" :placeholder="placeholder" :value="modelValue"
+           @input="$emit('update:modelValue', $event.target.value)"
+           v-bind="$attr"
+    >
+  </div>
+</template>
+
+<style scoped>
+.button {
+  padding: 0;
+  border: none;
+  background: none;
+}
+</style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,12 @@
 <script setup>
+import BaseInput from "@/components/BaseInput.vue";
+import {reactive} from "vue";
 
+const form = reactive({
+  name: '',
+  email: '',
+  password: '',
+})
 </script>
 
 <template>
@@ -8,32 +15,27 @@
       <form class="card card-md" action="." method="post">
         <div class="card-body">
           <h2 class="card-title text-center mb-4">Créer un nouveau compte</h2>
-          <div class="mb-3">
-            <label class="form-label">Nom</label>
-            <input name="name" type="text" class="form-control" placeholder="Votre nom">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Email</label>
-            <input name="email" type="email" class="form-control" placeholder="exemple@email.com">
-          </div>
-          <div class="mb-3">
-            <label class="form-label">Mot de passe</label>
-            <div class="input-group input-group-flat">
-              <input name="password" type="password" class="form-control"  placeholder="Mot de passe"  autocomplete="off">
-              <span class="input-group-text">
-                  <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="2" /><path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" /></svg>
-                  </a>
-                </span>
-            </div>
-          </div>
+          
+          <BaseInput
+            v-model="form.name" label="Nom" placeholder="Votre nom" type="text"
+          />
+          
+          <BaseInput
+            v-model="form.email" label="Email" placeholder="exemple@email.com" type="email"
+          />
+          
+          <BaseInput
+            v-model="form.password" label="Mot de passe" placeholder="Mot de passe" type="password"
+          />
+          
           <div class="form-footer">
             <button type="submit" class="btn btn-primary w-100">Créer mon compte</button>
           </div>
         </div>
       </form>
       <div class="text-center text-muted mt-3">
-        Vous avez déjà un compte? <router-link to="/login">connectez-vous</router-link>
+        Vous avez déjà un compte?
+        <router-link to="/login">connectez-vous</router-link>
       </div>
     </div>
   </div>
