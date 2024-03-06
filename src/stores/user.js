528de3a06ba3ba6f09c9d0a97dea6e24d640b852
jordan-nguyen <dizorder3@gmail.com>
2024-03-06 12:29:09 +0100

    ADD [Register]

---
+++ src/stores/user.js
@@ -0,0 +1,52 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+export const useUserStore = defineStore({
+    id: 'user',
+
+    state: () => ({
+        loggedIn : localStorage.getItem('token') ? true : false,
+        user : localStorage.getItem('user') ?? null,
+        token : localStorage.getItem('token') ?? false,
+        errors: [],
+    }),
+
+    getters: {
+        getToken : (state) => state.token,
+        getUser: (state) => JSON.parse(state.user),
+        getErrors : (state) => state.errors,
+    },
+
+    actions: {
+        async csrf() {
+            await axios.get('/sanctum/csrf-cookie');
+        },
+
+        async register(props) {
+            this.errors = [];
+            await this.csrf();
+            await axios.post('/register', props).then((response) => {
+                console.log(response);
+                if (response.status == 201) {
+                    const user = response.data.user;
+                    const token = response.data.token;
+                    localStorage.setItem('user', JSON.stringify(user))
+                    localStorage.setItem('token' , token)
+                    axios.defaults.headers.common['Authorization'] = token;
+                    this.loggedIn = true;
+                    this.user = user;
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                if (error.response.status == 422) {
+                    for (const  key in error.response.data.errors) {
+                        this.errors.push(error.response.data.errors[key][0] + ' ');
+                    }
+                    console.log(this.errors);
+                }
+            })
+        }
+    }
+})
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -2,6 +2,10 @@
 import BaseInput from "@/components/BaseInput.vue";
 import {reactive} from "vue";
 import axios from "axios";
+import { useUserStore } from '@/stores/user.js'
+import { useRouter } from "vue-router";
+
+const user = useUserStore();
 
 const form = reactive({
   name: '',
@@ -9,18 +13,11 @@ const form = reactive({
   password: '',
 })
 
-const submit = async () => {
-  console.log(form);
-  await axios.get('/sanctum/csrf-cookie');
-  
-  await axios.post('/register', form)
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
-    })
+const submit = () => {
+  user.register(form);
+  // rediriger
 }
+
 </script>
 
 <template>
@@ -30,21 +27,24 @@ const submit = async () => {
         <div class="card-body">
           <h2 class="card-title text-center mb-4">Créer un nouveau compte</h2>
           
+          <div v-if="user.getErrors.length" class="alert alert-danger" role="alert">
+            <p v-for="error in user.getErrors">{{ error }}</p>
+          </div>
+          
           <BaseInput
             v-model="form.name" label="Nom" placeholder="Votre nom" type="text"
           />
-          
           <BaseInput
             v-model="form.email" label="Email" placeholder="exemple@email.com" type="email"
           />
-          
           <BaseInput
             v-model="form.password" label="Mot de passe" placeholder="Mot de passe" type="password"
           />
-          
           <div class="form-footer">
             <button type="submit" class="btn btn-primary w-100">Créer mon compte</button>
           </div>
+          
+<!--            {{ form.messageSuccess }}-->
         </div>
       </form>
       <div class="text-center text-muted mt-3">
@@ -59,5 +59,8 @@ const submit = async () => {
 .card-title {
   font-size: 25px;
   font-weight: 700;
+}
+.success {
+  color: #0ca678;
 }
 </style>
