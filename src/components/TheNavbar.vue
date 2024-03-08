654589918fa8b37cd0b7c6ff5424a7af87ac2735
jordan-nguyen <dizorder3@gmail.com>
2024-03-08 08:45:06 +0100

    FIX [Register] add href

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -68,7 +68,7 @@ const user = useUserStore();
               </a>
             </li>
             <li v-if="user.loggedIn" @click.prevent="user.logout()" class="nav-item">
-              <a class="nav-link" href=".">
+              <a class="nav-link" href="/register">
                     <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-dashboard" width="24"
                            height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none"
