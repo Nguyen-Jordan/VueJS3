1d499a677a1c1901e874583395e47e0f3ffba439
jordan-nguyen <dizorder3@gmail.com>
2024-03-08 12:08:22 +0100

    UPD [PostView] add link on img

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { useRouter, useRoute } from "vue-router";
-import { storeToRefs } from "pinia";
-import { usePostStore } from "@/stores/post.js";
+import {useRouter, useRoute} from "vue-router";
+import {storeToRefs} from "pinia";
+import {usePostStore} from "@/stores/post.js";
 import moment from "moment";
 
 const router = useRouter();
@@ -9,9 +9,9 @@ const route = useRoute();
 
 const store = usePostStore();
 
-const { post, errors } = storeToRefs(store);
+const {post, errors} = storeToRefs(store);
 
-const { getPost } = store;
+const {getPost} = store;
 
 getPost(route.params.slug);
 
@@ -31,18 +31,20 @@ getPost(route.params.slug);
         </div>
       </div>
       
-      <img :data-src="post.photo.url" :alt="post.title" class="card-img-top lazyload" async="decoding">
-      
+      <a v-if="post.photo" :href="post.photo.url" target="_blank">
+        <img v-if="post.photo" :data-src="post.photo.url" :alt="post.title" class="card-img-top lazyload" async="decoding">
+      </a>
       <div class="card-body">
         <h2 class="card-title">{{ post.title }}</h2>
         <p class="card-text">{{ post.content }}</p>
-        <div>
-          <span class="avatar me-3 rounded" :style="{ backgroundImage: 'url(' + post.user.avatar.thumbnail_url + ')' }"></span>
+        <div v-if="post.user">
+          <span v-if="post.user.avatar" class="avatar me-3 rounded"
+                :style="{ backgroundImage: 'url(' + post.user.avatar.thumbnail_url + ')' }"></span>
           <div class="pt-2">{{ post.user.name }}</div>
           <div class="text-muted">{{ moment(post.created_at).fromNow() }}</div>
         </div>
       </div>
-      
+    
     </div>
   </div>
 </template>
